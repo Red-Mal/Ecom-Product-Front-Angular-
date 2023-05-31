e3e0ae0d3103f70e3d950285e0513514ee5c2ffa
redae <reda.elmallahi@gmail.com>
2023-05-31 11:07:30 +0100

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./components/products/products.component";
+import {HomeComponent} from "./components/home/home.component";
+import {ProductAddComponent} from "./components/products/product-add/product-add.component";
+import {ProductEditComponent} from "./components/products/product-edit/product-edit.component";
+
+const routes: Routes = [
+  {path:"products",component:ProductsComponent},
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"newProduct",component:ProductAddComponent},
+  {path:"editProduct/:id",component:ProductEditComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
